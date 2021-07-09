f4d7e0ca480c41e7d241848448c5196d1b4fec13
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2021-07-09 21:47:48 +0600

    React project created and header component added

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      
+      
+    </div>
+  );
+}
+
+export default App;
